895b5dd2988c4e995b12a4e79498022ce1b8aa8b
izudata <izumchy@gmail.com>
2025-05-28 13:54:35 -0600

    Update

--- notebooks/models.py
+++ notebooks/models.py
@@ -1,17 +1,3 @@
-# import torch.nn as nn
-# from torchvision import models
-
-# def get_model(name="mobilenet", num_classes=5, pretrained=True):
-#     if name == "mobilenet":
-#         model = models.mobilenet_v3_large(pretrained=pretrained)
-#         model.classifier[3] = nn.Linear(model.classifier[3].in_features, num_classes)
-#     elif name == "efficientnet":
-#         model = models.efficientnet_b4(pretrained=pretrained)
-#         model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-#     else:
-#         raise ValueError("Model name must be 'mobilenet' or 'efficientnet'")
-#     return model
-
 import torch.nn as nn
 from torchvision import models
 
